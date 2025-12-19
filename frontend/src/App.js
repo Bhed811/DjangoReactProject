259be697af62c75c523c1cb698c8ad2183ecf455
Bhed811 <dhairyasinghal.811@gmail.com>
2025-12-20 02:13:12 +0530

    Admin Sidebar Desing with Collapsible Menus

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import AdminLogin from './pages/AdminLogin';
+import AdminDashboard from './pages/AdminDashboard';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/admin-login" element={<AdminLogin/>}></Route>
+        <Route path="/admin-dashboard" element={<AdminDashboard/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/AdminSidebar.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import '../styles/admin.css'
+import { Link } from 'react-router-dom'
+import {FaCopy, FaEdit, FaSearch, FaThLarge, FaUserCheck, FaUserEdit, FaUsers} from 'react-icons/fa'
+const AdminSidebar = () => {
+
+    const [openMenus, setOpenMenus] = useState({
+        category: false,
+        food: false,
+        order: false
+    });
+
+    const toggleMenu = (menu) => {
+        setOpenMenus((prev) => ( {...prev, [menu]: !prev[menu] }));
+    };
+
+    return (
+        <div className="bg-dark text-white sidebar">
+            <div className="text-center p-3 border-bottom">
+                <img src="/images/admin.png" className="img-fluid rounded-circle mb-2" width="70"/>
+                <h6 className='mmb-0'>Admin</h6>      
+            </div>            
+
+            <div className='list-group list-group-flush'>
+                <Link className='list-group-item list-group-item-action bg-dark text-white'>
+                    <FaThLarge className='me-2 ' />Dashboard</Link>
+                
+                <div className='list-group list-group-flush'>
+                <Link className='list-group-item list-group-item-action bg-dark text-white'>
+                    <FaUsers className='me-2 '/>Reg Users</Link>
+            </div>
+
+            <button onClick={() => toggleMenu('category')} className='list-group-item list-group-item-action bg-dark text-white'>
+                <FaEdit className='me-2 '/>Food Category
+            </button>
+                {openMenus.category && (
+                    <div className='ps-4'>
+                <Link className='list-group-item list-group-item-action bg-dark text-white'>
+                    Add Category</Link>
+                
+                <Link className='list-group-item list-group-item-action bg-dark text-white'>
+                    Manage Category</Link>
+            </div>
+                )}
+            
+
+            <button onClick={() => toggleMenu('food')} className='list-group-item list-group-item-action bg-dark text-white'>
+                <FaEdit className='me-2 '/>Food Item
+            </button>
+                {openMenus.food && (
+                    <div className='ps-4'>
+                <Link className='list-group-item list-group-item-action bg-dark text-white'>
+                    Add Food Item</Link>
+                
+                <Link className='list-group-item list-group-item-action bg-dark text-white'>
+                    Manage Food Item</Link>
+            </div>
+                )}
+            
+
+            <button onClick={() => toggleMenu('order')} className='list-group-item list-group-item-action bg-dark text-white'>
+                <FaEdit className='me-2 '/>Orders
+            </button>
+                {openMenus.order && (
+                   <div className='ps-4'>
+                <Link className='list-group-item list-group-item-action bg-dark text-white'>
+                    Add Order</Link>
+                
+                <Link className='list-group-item list-group-item-action bg-dark text-white'>
+                    Manage Orders</Link>
+            </div> 
+                )}
+            
+
+            
+            <div className='list-group list-group-flush'>
+                <Link className='list-group-item list-group-item-action bg-dark text-white'>
+                    <FaSearch className='me-2'/>Search</Link>
+            </div>
+
+            <div className='list-group list-group-flush'>
+                <Link className='list-group-item list-group-item-action bg-dark text-white'>
+                    <FaEdit className='me-2'/>Manage Reviews</Link>
+            </div>
+
+                
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default AdminSidebar
