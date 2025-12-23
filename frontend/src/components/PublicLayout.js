f7988e40a02aebd5acdc9f8479ba38bb12fa3de4
Bhed811 <dhairyasinghal.811@gmail.com>
2025-12-24 02:47:18 +0530

    Logout functionality and conditional navbar designs

--- frontend/src/components/PublicLayout.js
+++ frontend/src/components/PublicLayout.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect} from 'react'
-import { FaHome, FaSignInAlt, FaTruck, FaUserPlus, FaUserShield, FaUtensils } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react'
+import { FaCogs, FaHeart, FaHome, FaShoppingCart, FaSignInAlt, FaSignOutAlt, FaTruck, FaUser, FaUserCircle, FaUserPlus, FaUserShield, FaUtensils } from 'react-icons/fa'
 import { Link, useNavigate } from 'react-router-dom'
 import '../styles/layout.css';
 const PublicLayout = ({ children }) => {
@@ -10,7 +10,7 @@ const PublicLayout = ({ children }) => {
   const navigate = useNavigate();
   const userId = localStorage.getItem('userId');
   const name = localStorage.getItem('userName');
-  
+
   useEffect(() => {
     if (userId) {
       setIsLoggedIn(true);
@@ -19,46 +19,77 @@ const PublicLayout = ({ children }) => {
       setIsLoggedIn(false);
       setUserName('');
     }
-  }, [userId]); 
+  }, [userId]);
+
+  const handleLogout = () => {
+    localStorage.removeItem('userId');
+    localStorage.removeItem('userName');
+    setIsLoggedIn(false);
+    navigate('/login');
+  }
 
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container">
-    <Link className="navbar-brand fw-bold" to="#"><FaUtensils className='me-2 '/> Food Ordering System</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto">
-        <li className="nav-item ms-1">
-          <Link className="nav-link " aria-current="page" to="/"><FaHome className='me-1 '/>Home</Link>
-        </li>
-        <li className="nav-item mx-1">
-          <Link className="nav-link" to="#"><FaUtensils className='me-1 '/>Menu</Link>
-        </li>
-        <li className="nav-item mx-1">
-          <Link className="nav-link" to="#"><FaTruck className='me-1 '/>Track</Link>
-        </li>
-        <li className="nav-item mx-1">
-          <Link className="nav-link" to="/register"><FaUserPlus className='me-1 '/>Register</Link>
-        </li>
-        <li className="nav-item mx-1">
-          <Link className="nav-link" to="/login"><FaSignInAlt className='me-1 '/>Login</Link>
-        </li>
-        <li className="nav-item mx-1">
-          <Link className="nav-link" to="/admin-login"><FaUserShield className='me-1 '/>Admin</Link>
-        </li>
-        <li className="nav-item mx-1">
-          <Link className="nav-link" to="/admin-login"><FaUserShield className='me-1 '/>{userName}</Link>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-          </nav>
-          
-         <div className='text-primary'>{children}</div>
+        <div className="container">
+          <Link className="navbar-brand fw-bold" to="#"><FaUtensils className='me-2 ' /> Food Ordering System</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item ms-1">
+                <Link className="nav-link " aria-current="page" to="/"><FaHome className='me-1 ' />Home</Link>
+              </li>
+              <li className="nav-item mx-1">
+                <Link className="nav-link" to="#"><FaUtensils className='me-1 ' />Menu</Link>
+              </li>
+              <li className="nav-item mx-1">
+                <Link className="nav-link" to="#"><FaTruck className='me-1 ' />Track</Link>
+              </li>
+
+              {!isLoggedIn ? (
+                <> <li className="nav-item mx-1">
+                  <Link className="nav-link" to="/register"><FaUserPlus className='me-1 ' />Register</Link>
+                </li>
+                  <li className="nav-item mx-1">
+                    <Link className="nav-link" to="/login"><FaSignInAlt className='me-1 ' />Login</Link>
+                  </li>
+                  <li className="nav-item mx-1">
+                    <Link className="nav-link" to="/admin-login"><FaUserShield className='me-1 ' />Admin</Link>
+                  </li> </>
+              ) : (<>
+                <li className="nav-item mx-1">
+                  <Link className="nav-link" to=""><FaUser className='me-1 ' />My Orders</Link>
+                </li>
+                <li className="nav-item mx-1">
+                  <Link className="nav-link" to=""><FaShoppingCart className='me-1 ' />Cart</Link>
+                </li>
+                <li className="nav-item mx-1">
+                  <Link className="nav-link" to=""><FaHeart className='me-1 ' />Wishlist</Link>
+                </li>
+
+                <li class="nav-item dropdown text-capitalize">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
+                    <FaUserCircle className='me-1 ' />{userName}
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><Link class="dropdown-item" to="#"><FaUser className='me-1 ' />Profile</Link></li>
+                    <li><Link class="dropdown-item" to="#"><FaCogs className='me-1 ' />Settings</Link></li>
+                    <li><hr class="dropdown-divider" /></li>
+                    <li><button class="dropdown-item" onClick={handleLogout}><FaSignOutAlt className='me-1 ' />Logout</button></li>
+                  </ul>
+                </li>
+              </>)}
+
+
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+
+      <div className='text-primary'>{children}</div>
 
       <footer className='text-center py-3 mt-5'>
         <div className='container '>
